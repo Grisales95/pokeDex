9513036e93af20544430c447f614494e653b0044
Grisales95 <grisales-95@outlook.es>
2021-06-10 00:11:49 -0500

    FireBase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import pokemon from "../img/pokemon.png";
 import { Link } from "react-router-dom";
+import { auth } from "../firebase"
 
 function Login() {
-  const enviar = (e) => {
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+
+  const RegisterUser = (e) => {
     e.preventDefault();
+    try{
+      auth.createUserWithEmailAndPassword(email, pass)
+      alert("usuario registrado")
+    } catch(error){
+        console.log(error)
+    }
   };
 
   return (
@@ -13,14 +23,23 @@ function Login() {
         {<img src={pokemon} alt="pokemon-logo" />}
       </div>
       <div className="container-form">
-        <form className="card p-3 text-center" onSubmit={enviar}>
+        <form className="card p-3 text-center" onSubmit={RegisterUser}>
           <span>Login </span>
           <input
-            type="text"
+            onChange = {(e)=> {setEmail(e.target.value)}}
+            type="email"
             className="form-control my-3"
-            placeholder="Your Name..."
+            placeholder="Your Email..."
             required
           />
+          <input
+            onChange = {(e)=> {setPass(e.target.value)}}
+            type="password"
+            className="form-control my-3"
+            placeholder="Your Password..."
+            required
+          />
+          <button className="btn my-2" type="submit">Registrar usuario</button>
           <Link className="btn" to="/pokeDex">
             Go
           </Link>
